cecd80d8c03d3ddae8542843a206525c148978f0
not-ayan <notayan69@gmail.com>
2025-04-14 20:18:27 +0530

    disable no used errors

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable "defined but not used" errors
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-unused-vars": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
